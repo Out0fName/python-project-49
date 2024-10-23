fa16634e5b462422db77848c1e51c053402e6642
Nikita Serdyukov <serdukovnikita@yandex.ru>
2024-10-23 21:52:28 +0300

    five step in progress

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,27 +2,19 @@
 
 
 import prompt
-import randome
+import random
 
 
-def welcome_user():
-        name = prompt.string('May I have you name?  ')
-        print('Hello, ' + name + '!')
-
-
-def main():
-        print("Welcome to the Brain Games!")
-
-
-if __name__ == '__main__':
-     main()
-
-welcome_user()
-
+print("Welcome to the Brain Games!")
+name = prompt.string('May I have you name?  ')
+print('Hello, ' + name + '!')
 print('Answer "yes" if number is even, otherwise answer "no".')
+
 attempts = 3
-for count in range(attempts):
-    number = randome.randint(1, 1000)
+count = 0
+
+while count < attempts:
+    number = random.randint(1, 1000)
     print('Question: ' + str(number))
     if number % 2 == 0:
         answer = 'yes'
@@ -35,6 +27,7 @@ for count in range(attempts):
         print("'" + user_answer + "' is wrong answer ;(. Correct answer was '" + answer + "'.")
         print("Let's try again, " + name + "!")
         break
+    count += 1
 
 if count == attempts:
     print('Congratulations, ' + name + '!')
